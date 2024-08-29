81f3d5ec7454c9b5a4f252553ea9212150832e07
Ajith637464 <ajithuk112233@gmail.com>
2024-08-29 17:07:13 +0530

    Update Find_Frequency_Of_The_String.java

--- Find_Frequency_Of_The_String.java
+++ Find_Frequency_Of_The_String.java
@@ -7,27 +7,25 @@ Output: j : 2
 import java.util.Scanner;
 class Find_Frequency_Of_The_String
 {
+	static Scanner sc = new Scanner (System.in);
 	public static void solve(String s) 
 	{
-			char[]ch=s.toCharArray();
-			for (int i=0;i<ch.length ;i++ )
+		char[]ch=s.toCharArray();
+		for (int i=0;i<ch.length ;i++ )
+		{
+			int count=1;
+			for (int j=i+1;j<ch.length ;j++ )
 			{
-				int count=1;
-				for (int j=i+1;j<ch.length ;j++ )
+				if (ch[i]==ch[j] && ch[i] != ' ')
 				{
-					if (ch[i]==ch[j] && ch[i] != ' ')
-					{
-						count++;
-						ch[j]=' ';
-					}
+					count++;
+					ch[j]=' ';
 				}
-				if (ch[i] != ' ')
-					System.out.println(ch[i]+" : "+count);
 			}
-		
-		
+			if (ch[i] != ' ')
+				System.out.println(ch[i]+" : "+count);
+		}	
 	}
-	static Scanner sc = new Scanner (System.in);
 	public static void main(String[] args) 
 	{
 		System.out.print("Input:");
